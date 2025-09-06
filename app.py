8e4c374e4e8a4a374900a8fd406d590993f99144
Daniel Aguayo <1136475+dantoac@users.noreply.github.com>
2025-09-06 08:54:07 -0400

    Refactor naming of `Roles` and `Permissions` classes to `UserGroups` and `Authorizations`.

--- app.py
+++ app.py
@@ -38,24 +38,24 @@ class Users:
     role: int
 
 
-class Roles:
+class UserGroups:
     id: int
     name: str = "User"
 
 
-class Permissions:
+class Authorizations:
     id: int
     name: str
-    role_id: int
+    group_id: int
 
 
-roles = db.create(Roles, pk="id", transform=True, not_null={"name"})
+roles = db.create(UserGroups, pk="id", transform=True, not_null={"name"})
 roles.upsert(id=1, name="Admin")
 roles.upsert(id=2, name="User")
 users = db.create(
     Users, pk="email", transform=True, not_null={"uuid"}, defaults=dict(role=2)
 )
-permissions = db.create(Permissions, pk="id", transform=True)
+permissions = db.create(Authorizations, pk="id", transform=True)
 
 
 def verify_password(plain_password: str = None, hashed_password: str = None) -> bool:
